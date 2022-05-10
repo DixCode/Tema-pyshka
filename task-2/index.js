9b320710c05e1e389337570b3b8d472a3e341b25
Valery_Dubovets <dixxysss@gmail.com>
2022-05-10 22:36:38 +0300

    feat:done task 2

---
+++ task-2/index.js
@@ -0,0 +1,30 @@
+//customSome
+const array = [1,2,3, 4, 5, 6, 7, 8, 9];
+Array.prototype.customSome = function(callback) {
+  if (this.length === 0) return false;
+  let result=false;
+  for (let i = 0; i < this.length; i++) {
+    if(callback(this[i])) {
+      result=true
+    }
+  }
+  return result;
+};
+
+console.log(array.customSome(item => item === 2));
+
+//customEvery
+
+const array1=[2,2,2,2,2]
+Array.prototype.customEvery = function(callback) {
+  if (this.length === 0) return true;
+  let result = true;
+  for (let i = 0; i < this.length; i++) {
+    if (!callback(this[i])) {
+      result = false;
+    }
+  }
+  return result;
+};
+
+console.log(array1.customEvery(item => item === 2));
